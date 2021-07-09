9b07acf31a75e04731c715c6af2ba4822ec36523
mehmet ali ipsuz <mehmetaliipsuz@hotmail.com>
2021-07-09 16:35:57 -0500

    added new features

--- bootloader/main.s
+++ bootloader/main.s
@@ -1,9 +1,9 @@
+
 /*
- * SPI Bootloader for Atmega328p
+ * main.s
  *
  * Created: 7/7/2021 8:21:28 PM
- * Author: M. Ali Ipsuz
- * Toolchain: AVR-GCC
+ *  Author: mehme
  */ 
 
 #include "inc/io.h"
@@ -31,21 +31,81 @@
 			Set BLBSET and SELFPRGEN in SMPCSR
 			Read Z register within 3 cycles with LPM. So disable interrupts first.
 		*/
-		cli
+		cli	;Disable interrupts
 		ldi r30, 0x03	;Set Z = 0x0003
 		ldi r31, 0x00
 		ldi r16, (1<<BLBSET) | (1<<SELFPRGEN)	;Set BLBSET = 1, SELFPRGEN = 1
 		out _SFR_IO_ADDR(SPMCSR), r16	;Write BLBSET and SELFPRGEN to SPMCSR
 		lpm r17, Z
-		sei
+		;sei	;Enable interrupts
 
-		sts hfuse, r17
+		sts hfuse, r17	;Store FUSE_HIGH in SRAM
+		
+		/*Is bootsize large enough?*/
 		andi r17, bootsz_filter
 		cpi r17, desired_boot_size
+		brne not_desired_bootsize	;Branch if it is not large enough.
 
-	error:
+		;Erase the page of 0x00
+		ldi ZL, 0x00
+		ldi ZH, 0x00
+		call erase_page
+
+		call wait_for_spm	;Wait SPM command to be executed.
+
+		/*Load buffer with data*/
+		ldi ZL, 0x00
+		ldi ZH, 0x00
+		ldi r18, 0x0C
+		ldi r19, 0x94
+		movw r18,r0
+		ldi r16, 0x01
+		out _SFR_IO_ADDR(SPMCSR), r16
+		spm
+
 		
+		/*Clear the RWWSB bit
+		ldi r16, 0x01
+		out _SFR_IO_ADDR(SPMCSR), r16
+		spm*/
 
+		;Write page
+		ldi ZL, 0x00
+		ldi ZH, 0x00
+		ldi r16, 0x05
+		out _SFR_IO_ADDR(SPMCSR), r16
+		spm
 
+		nop
+		nop
+		nop
 
+	;This subroutine waits until SPMEN bit in SPMCSR is cleared.
+	;It uses r16, so it should be pushed to stack before run through.
+	wait_for_spm:
+		push r16
+		.loop:
+			in r16, _SFR_IO_ADDR(SPMCSR)
+			sbrc r16, SPMEN
+			rjmp .loop
+		pop 16
+		ret
 
+	;This subroutine erase the entire page pointed on Z register PCPAGE segment.
+	;Z register bits are segmented as follows.
+	;PCWORD = Z6:Z1
+	;PCPAGE = Z14:Z7
+	;Z0, Z15 are "don't care" bits.
+	;Before calling this subroutine Z register must be arranged.
+	;This subroutine uses r16, so it should be pushed to stack before run through.
+	erase_page:
+		push r17
+		ldi r17, (1 << PGERS) | (1 << SPMEN)
+		out _SFR_IO_ADDR(SPMCSR), r16
+		spm
+		pop r17
+		ret
+
+	not_desired_bootsize:
+		nop
+		nop
